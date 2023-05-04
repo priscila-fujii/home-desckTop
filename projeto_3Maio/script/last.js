6ba9b90752a06a0bc405466c1399ab6c7162ba68
priscila-yone-fujii <guingitsune78@gmail.com>
2023-05-04 08:53:40 -0300

    proximo exercicio em while/dowhile

--- projeto_3Maio/script/last.js
+++ projeto_3Maio/script/last.js
@@ -1,13 +1,3 @@
-let corpoDoc;
-let docCreator;
-let tagH1;
 
-setTimeout(function demorar(){
-    corpoDoc = document.querySelector('.corpo');
-    docCreator = document.createElement('p');
-    tagH1 = document.getElementById("h1").style.color = '#ffe478';
-    docCreator.textContent = "Novo Mundoooo";
-    corpoDoc.appendChild(docCreator);
-}, 6000);
 
 // -------------------------------------
